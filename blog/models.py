aae907a25b3509c6d3b5421d27efdd434c6c1093
Marten <mgransow@gmail.com>
2021-02-03 21:47:30 +0100

    blog seite fertig. static files added

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,17 @@ class Blog(models.Model):
    pub_date = models.DateTimeField(auto_now=None,auto_now_add=None)
    body = models.TextField()
    image = models.ImageField(upload_to='images')
+
+   def summary(self):
+      sumary = self.body[:100]
+      if len(self.body) > 100:
+         return sumary + '...'
+      else:
+         return self.body
+
+   
+   def pub_date_pretty(self):
+      return self.pub_date.strftime('%b %e %Y')
+   
+   def __str__(self):
+      return self.title
