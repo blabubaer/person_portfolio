195f4dd248a1f7e79ecf9c267efd0615bb4d6b6c
Marten <mgransow@gmail.com>
2021-02-04 10:13:19 +0100

    final touches

--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 class Job(models.Model):
+    title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    git_page = models.URLField()
+    viewer = models.URLField(default='https://blabubaer.github.io/start-it-module1/')
 
+    def __str__(self):
+      return self.title
